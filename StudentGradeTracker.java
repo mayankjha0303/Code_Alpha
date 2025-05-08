6459a7dce77d5ca9a02433e0fcab73bbe4fba3e4
Mayank jha <jhamayank988@gmail.com>
2025-05-08 23:09:53 +0530

    Add files via upload

---
+++ StudentGradeTracker.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentGradeTracker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> grades = new ArrayList<>();
+        
+        System.out.println("=== Student Grade Tracker ===");
+        
+        System.out.print("Enter the number of students: ");
+        int numberOfStudents = scanner.nextInt();
+        
+       
+        for (int i = 0; i < numberOfStudents; i++) {
+            System.out.print("Enter grade for student " + (i + 1) + ": ");
+            int grade = scanner.nextInt();
+            grades.add(grade);
+        }
+        
+       
+        int sum = 0;
+        int highest = grades.get(0);
+        int lowest = grades.get(0);
+        
+        for (int grade : grades) {
+            sum += grade;
+            if (grade > highest) {
+                highest = grade;
+            }
+            if (grade < lowest) {
+                lowest = grade;
+            }
+        }
+        
+        double average = (double) sum / numberOfStudents;
+        
+       
+        System.out.println("\n=== Grade Summary ===");
+        System.out.println("Average Grade: " + average);
+        System.out.println("Highest Grade: " + highest);
+        System.out.println("Lowest Grade: " + lowest);
+        
+        scanner.close();
+    }
+}
+
